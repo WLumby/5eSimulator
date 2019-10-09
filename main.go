dd41b19cb742035bd10c97c672d281c5f58415a7
Will Lumby <wlumby@gmail.com>
2019-10-09 10:08:24 +0100

    5ESIM-6 Start testing attack round in main

--- main.go
+++ main.go
@@ -12,16 +12,39 @@ func main() {
 		fmt.Println(err)
 	}
 
-	attackRoll, attackDamage, damageType, crit := rollWeapon(playerCharacter)
-	if crit {
-		fmt.Printf("Attack Roll: %v (CRIT!), Damage: %v %v\n", attackRoll, attackDamage, damageType)
-	} else {
-		fmt.Printf("Attack Roll: %v, Damage: %v %v\n", attackRoll, attackDamage, damageType)
-	}
+	dead := false
 
-	attackRoll, attackDamage, damageType, crit = rollWeapon(enemy)
-	if crit {
-		fmt.Printf("Attack Roll: %v (CRIT!), Damage: %v %v\n", attackRoll, attackDamage, damageType)
-	} else {
-		fmt.Printf("Attack Roll: %v, Damage: %v %v\n", attackRoll, attackDamage, damageType)
-	}}
+	for !dead {
+		attackRoll, attackDamage, damageType, crit := rollWeapon(playerCharacter)
+		if crit {
+			fmt.Printf("%v --- Attack Roll: %v (CRIT!), Damage: %v %v\n", playerCharacter.Name, attackRoll, attackDamage, damageType)
+		} else {
+			fmt.Printf("%v --- Attack Roll: %v, Damage: %v %v\n", playerCharacter.Name, attackRoll, attackDamage, damageType)
+		}
+
+		if attackRoll >= int(enemy.ArmourClass) {
+			
+		}
+
+		fmt.Printf("Health: ")
+		for i := 0; i < int(playerCharacter.Health); i++ {
+			fmt.Printf("-")
+		}
+		fmt.Println("\n")
+
+		attackRoll, attackDamage, damageType, crit = rollWeapon(enemy)
+		if crit {
+			fmt.Printf("%v --- Attack Roll: %v (CRIT!), Damage: %v %v\n", enemy.Name, attackRoll, attackDamage, damageType)
+		} else {
+			fmt.Printf("%v --- Attack Roll: %v, Damage: %v %v\n", enemy.Name, attackRoll, attackDamage, damageType)
+		}
+
+		fmt.Printf("Health: ")
+		for i := 0; i < int(enemy.Health); i++ {
+			fmt.Printf("-")
+		}
+		fmt.Println("\n")
+
+		dead = true
+	}
+}
