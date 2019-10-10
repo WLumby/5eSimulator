6dccfe5270478e904521bb62227002ffc1a21d57
Will Lumby <wlumby@gmail.com>
2019-10-10 16:52:23 +0100

    5ESIM-7 Complete attack round in main with winner

--- main.go
+++ main.go
@@ -23,14 +23,16 @@ func main() {
 		}
 
 		if attackRoll >= int(enemy.ArmourClass) {
-			
+			fmt.Printf("HIT on %v for %v %v damage!\n\n", enemy.Name, attackDamage, damageType)
+			enemy.Health = int32(int(enemy.Health) - attackDamage)
+			if enemy.Health <= 0 {
+				dead = true
+				fmt.Printf("%v dies!", enemy.Name)
+				break
+			}
+		} else {
+			fmt.Printf("MISS on %v!\n\n", enemy.Name)
 		}
-
-		fmt.Printf("Health: ")
-		for i := 0; i < int(playerCharacter.Health); i++ {
-			fmt.Printf("-")
-		}
-		fmt.Println("\n")
 
 		attackRoll, attackDamage, damageType, crit = rollWeapon(enemy)
 		if crit {
@@ -39,12 +41,28 @@ func main() {
 			fmt.Printf("%v --- Attack Roll: %v, Damage: %v %v\n", enemy.Name, attackRoll, attackDamage, damageType)
 		}
 
-		fmt.Printf("Health: ")
+		if attackRoll >= int(playerCharacter.ArmourClass) {
+			fmt.Printf("HIT on %v for %v %v damage!\n\n", playerCharacter.Name, attackDamage, damageType)
+			playerCharacter.Health = int32(int(playerCharacter.Health) - attackDamage)
+			if playerCharacter.Health <= 0 {
+				dead = true
+				fmt.Printf("%v dies!", playerCharacter.Name)
+				break
+			}
+		} else {
+			fmt.Printf("MISS on %v!\n\n", playerCharacter.Name)
+		}
+
+		fmt.Printf("\nPlayer Health: ")
+		for i := 0; i < int(playerCharacter.Health); i++ {
+			fmt.Printf("-")
+		}
+		fmt.Println()
+
+		fmt.Printf("Enemy  Health: ")
 		for i := 0; i < int(enemy.Health); i++ {
 			fmt.Printf("-")
 		}
 		fmt.Println("\n")
-
-		dead = true
 	}
 }
